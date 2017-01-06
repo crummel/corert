fecdf508a41fc5ca5216a79c5b77321322835438
Chris Rummel <crummel@microsoft.com>
2017-01-06 17:01:09 -0600

    wip

---
+++ buildscripts/publish-packages.cmd
@@ -0,0 +1,32 @@
+REM don't pass args to buildvars-setup, just get defaults
+call %~dp0buildvars-setup.cmd
+
+set AzureAccount=
+set AzureToken=
+set Container=
+
+:Arg_Loop
+if "%1" == "" goto ArgsDone
+
+if /i "%1" == "-AzureAccount" (set AzureAccount=%2&shift&shift&goto Arg_Loop)
+if /i "%1" == "-AzureToken" (set AzureToken=%2&shift&shift&goto Arg_Loop)
+if /i "%1" == "-Container" (set Container=%2&shift&shift&goto Arg_Loop)
+
+echo Invalid command line argument: %1
+exit /b 1
+:ArgsDone
+
+if "%AzureAccount%" == "" (
+    echo Azure account not specified.
+    exit /b 1
+)
+if "%AzureToken%" == "" (
+    echo Azure token not specified.
+    exit /b 1
+)
+if "%Container%" == "" (
+    echo Azure container not specified.
+    exit /b 1
+)
+
+%_msbuildexe% %~dp0publish.proj /p:CloudDropAccountName=%AzureAccount% /p:CloudDropAccessToken=%AzureToken% /p:ContainerName=%Container% /flp:v=diag;LogFile=publish-packages.log
---
+++ buildscripts/publish-packages.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# don't pass args to buildvars-setup, just get defaults
+scriptRoot="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+. $scriptRoot/buildvars-setup.sh
+
+set AzureAccount=
+set AzureToken=
+set Container=
+
+while [ "$1" != "" ]; do
+        lowerI="$(echo $1 | awk '{print tolower($0)}')"
+        case $lowerI in
+        -azureaccount)
+            shift
+            set AzureAccount=$1
+            ;;
+        -azuretoken)
+            shift
+            set AzureToken=$1
+            ;;
+        -container)
+            shift
+            set Container=$1
+            ;;
+        *)
+          echo Bad argument $1
+          exit 1
+    esac
+    shift
+done
+
+$__ProjectRoot/Tools/msbuild.sh $scriptRoot/publish.proj /p:CloudDropAccountName=%AzureAccount% /p:CloudDropAccessToken=%AzureToken% /p:ContainerName=%Container% "/flp:v=diag;LogFile=publish-packages.log"
