357ec99bd5ce50de0c00ea95f4a6e58254e4aaa3
Chris Rummel <crummel@microsoft.com>
2017-01-09 12:34:24 -0600

    wip

--- buildscripts/publish-packages.sh
+++ buildscripts/publish-packages.sh
@@ -1,30 +1,22 @@
 #!/usr/bin/env bash
 
-echo "* $*"
-echo "0 $0 1 $1 2 $2 3 $3 4 $4 5 $5 6 $6 7 $7 8 $8 9 $9"
-
 export AzureAccount=
 export AzureToken=
 export Container=
-echo "* $*"
-echo "0 $0 1 $1 2 $2 3 $3 4 $4 5 $5 6 $6 7 $7 8 $8 9 $9"
 
 while [ "$1" != "" ]; do
         lowerI="$(echo $1 | awk '{print tolower($0)}')"
         case $lowerI in
         -azureaccount)
             shift
-            echo "account: $1"
             export AzureAccount=$1
             ;;
         -azuretoken)
             shift
-            echo "token: $1"
             export AzureToken=$1
             ;;
         -container)
             shift
-            echo "container: $1"
             export Container=$1
             ;;
         *)
@@ -38,5 +30,4 @@ done
 scriptRoot="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 . $scriptRoot/buildvars-setup.sh
 
-echo "./Tools/msbuild.sh $scriptRoot/publish.proj /p:CloudDropAccountName=$AzureAccount /p:CloudDropAccessToken=$AzureToken /p:ContainerName=$Container"
-./Tools/msbuild.sh $scriptRoot/publish.proj /p:CloudDropAccountName=$AzureAccount /p:CloudDropAccessToken=$AzureToken /p:ContainerName=$Container "/flp:v=diag;LogFile=publish-packages.log"
+$__ProjectRoot/Tools/msbuild.sh $scriptRoot/publish.proj /p:CloudDropAccountName=$AzureAccount /p:CloudDropAccessToken=$AzureToken /p:ContainerName=$Container "/flp:v=diag;LogFile=publish-packages.log"
